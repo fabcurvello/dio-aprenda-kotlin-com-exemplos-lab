2b95ff073dd3d49ba525b041fc5a8bfda7875a05
Fabricio Curvello <fabricio.desenvolvedor@gmail.com>
2024-06-20 21:33:47 -0300

    Desafio pronto

--- desafio.kt
+++ desafio.kt
@@ -2,20 +2,52 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario ( val nome: String )
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(var nomeCEduc: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nomeFormacao: String, var conteudos: List<ConteudoEducacional>, val nivel: Nivel) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        // Adiciona usuário à lista de inscritos
+        inscritos.add(usuario)
+        println("Usuário ${usuario.nome} mariculado na formação $nomeFormacao.")
     }
+
+    fun imprimirDetalhes() {
+        println("Formação: $nomeFormacao")
+        println("Nível: $nivel")
+        println("Conteúdos: ")
+        conteudos.forEach {
+            println("- ${it.nomeCEduc}: ${it.duracao} minutos")
+        }
+        println("Inscritos:")
+        inscritos.forEach {
+            println("- ${it.nome}")
+        }
+    }
+
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    //Criação de alguns conteúdos educacionais
+    val conteudo1 = ConteudoEducacional("Kotlin Básico", 120)
+    val conteudo2 = ConteudoEducacional("Kotlin Intermediário")
+    val conteudo3 = ConteudoEducacional("Kotlin Avançado", 100)
+
+    //Criação de uma formação
+    val formacaoKotlin = Formacao("Formacao Kotlin", listOf(conteudo1, conteudo2, conteudo3), Nivel.DIFICIL)
+
+    //Criação de usuários
+    val usuario1 = Usuario("Ana")
+    val usuario2 = Usuario("Pedro")
+
+    // Matrícula de usuários na formação
+    formacaoKotlin.matricular(usuario1)
+    formacaoKotlin.matricular(usuario2)
+
+    //Exibir detalhes da formação
+    formacaoKotlin.imprimirDetalhes()
 }
